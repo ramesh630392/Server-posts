4dd82f48cd09cedba68202901ab26f9b5e599f43
ramesh630392 <ramesh9912599@gmail.com>
2025-03-19 11:58:37 +0530

    .

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv';
 
 export const connectDb = async() =>{
+    dotenv.config();
+    const mongoURI = process.env.mongoURI
     try{
-
-        const conn = await mongoose.connect('mongodb+srv://Ramesh:zzjCdNL2aQznkR76@cluster0.bqurm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        const conn = await mongoose.connect(mongoURI);
         console.log('Mongoose is Connected');
-
     }catch(err){
         console.log(err);
         process.exit(1);
